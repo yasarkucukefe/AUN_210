be2ed25c71f30ce468fb246ed1f4b15e7539c40a
Yasar Kucukefe <yasar.kucukefe@gmail.com>
2024-05-03 12:16:08 +0300

    Hafta 12

---
+++ hafta-12/app.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const jwt = require('jsonwebtoken');
+const formidable = require('formidable');
+const multer = require("multer");
+
+const app = express()
+const port = 3000
+
+const gizli_anahtar = "WIEKDAKDPoD93929231"; // gizli olması gerekir. Koda yazılmaz.
+
+app.use(express.static('public')); // public klasöründe yer alan dosyaları servis etmek için.
+
+
+const multer2 = multer();
+app.use(multer2.none())
+app.get("/jwt", async(req, res, next) => {
+
+    const {token} = req.body    
+    if(err){res.send("Form hatası"); res.end()}
+  
+    console.log(token);
+    const dogrula = jwt.verify(token, gizli_anahtar);
+    if(dogrula){
+        res.send("JWT Token geçerli.")
+    }else{
+        res.send("Hatalı JWT kodu.");
+    }
+
+});
+
+app.post("/jwt", async(req, res, next) => {
+    const form = new formidable.IncomingForm();
+    form.parse( req, function(err, fields, files){
+        if(err){res.send("Form hatası"); res.end()}
+        const okulNo = fields.okulNo;
+        const email = fields.email;
+
+        let data = {
+            okul_no: okulNo,
+            ePosta: email
+        }
+
+        const token = jwt.sign(data, gizli_anahtar);
+
+        res.send(token);
+           
+    })
+})
+
+app.delete("/user", (req, res) => {
+    res.send("DELETE methodu ile gönderilen işlem yapılıyor...");
+})
+
+
+app.get("/user", (req, res) => {
+    res.send("GET ile gönderilen parametreler işleniyor...");
+})
+
+app.post("/user", (req, res) => {
+    res.send("POST ile gönderilen parametreler işleniyor...");
+})
+
+
+// https://jwt.io/introduction
+// JWT: JSON Web Token
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
